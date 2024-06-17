dccb45aa32df114cb6fdfd965b4cab780f99aa62
Acr777 <115130508+Acr777@users.noreply.github.com>
2024-06-17 12:51:40 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+self.addEventListener('push', function(event) {
+  const data = event.data.json();
+  const options = {
+      body: data.body,
+      icon: '2.png', // You can specify a notification icon here
+      data: {
+          url: data.url
+      }
+  };
+  event.waitUntil(
+      self.registration.showNotification(data.title, options)
+  );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+      clients.matchAll({ type: 'window' }).then(function(clientList) {
+          for (var i = 0; i < clientList.length; i++) {
+              var client = clientList[i];
+              if (client.url == event.notification.data.url && 'focus' in client) {
+                  return client.focus();
+              }
+          }
+          if (clients.openWindow) {
+              return clients.openWindow(event.notification.data.url);
+          }
+      })
+  );
+});
